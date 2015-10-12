a3bc27d02497e2f6f7a7f607941c69345038183d
Reid Bixler <rmb3yz@virginia.edu>
2015-10-12 17:23:07 -0400

    Added Files
    added python lab demo files

---
+++ dev/hello.py
@@ -0,0 +1,10 @@
+__author__ = 'Reid'
+
+from helper import greeting
+
+def main():
+    greeting("hello")
+    return 0
+
+if __name__ == "__main__":
+    main()
---
+++ dev/helper.py
@@ -0,0 +1,6 @@
+__author__ = 'Reid'
+
+
+def greeting(msg):
+    print(msg)
+    return

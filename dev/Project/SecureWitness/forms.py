134555731445fe57b4d3bf984b118b8c4a429248
Mike Wang <mlw3nq@virginia.edu>
2015-12-06 23:07:48 -0500

    Still merging

--- dev/Project/SecureWitness/forms.py
+++ dev/Project/SecureWitness/forms.py
@@ -18,14 +18,12 @@ from Project.SecureWitness.models import *
 
 DEFAULT_STOPWORDS = 'a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your'
 
-<<<<<<< HEAD
+
 if hasattr(settings, 'DATABASES'):
     DATABASE_ENGINE =  settings.DATABASES[list(settings.DATABASES.keys())[0]]['ENGINE'].split('.')[-1]
 else:
     DATABASE_ENGINE =  settings.DATABASE_ENGINE
-=======
-DATABASE_ENGINE =  settings.DATABASES['default']['ENGINE']
->>>>>>> 5e9f38e2f029990d895a18771d5967a708fd4eb3
+
 
 
 class ReportForm(forms.ModelForm):
@@ -237,7 +235,7 @@ class BaseSearchForm(forms.Form):
 class ReportSearchForm(BaseSearchForm):
     class Meta:
 
-        base_qs = Document.objects
+        base_qs = Report.objects
         search_fields = ('^title', 'description', 'user', '=id')
 
 
